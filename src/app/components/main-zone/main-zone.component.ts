0445ef724872e989a2afa1eb99bb230f4b8da853
Amit Shwarts <amit.shwarts@gmail.com>
2021-03-24 22:21:25 +0200

    improve ViewService & MainZone management of Views.

--- src/app/components/main-zone/main-zone.component.ts
+++ src/app/components/main-zone/main-zone.component.ts
@@ -1,20 +1,31 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { ViewService } from '../../services/view-service/view.service';
+import { NodeView, ViewPosition } from '../../../assets/types';
 
 @Component({
   selector: 'app-main-zone',
   templateUrl: './main-zone.component.html',
   styleUrls: ['./main-zone.component.scss']
 })
-export class MainZoneComponent implements OnInit{
-  constructor(public vService: ViewService){
+export class MainZoneComponent implements OnInit, DoCheck
+{
+  public readonly viewsArray: ReadonlyArray<NodeView>;
+  private getAllViewsState = { time: 100, toggle: true };
+
+  constructor(public vService: ViewService)
+  {
+    this.viewsArray = this.vService.getAll();
   }
 
-  ngOnInit(): void{
-  }
+  ngOnInit(): void{}
+  ngDoCheck(): void{}
 
-  drop(viewId: number, distance: { x: number, y: number }): void{
-    // TODO : check if objects overlay
+  drop(viewId: number, distance: ViewPosition): void
+  {
     this.vService.update(viewId, distance);
+  }
+  deleteView(viewId: number): void
+  {
+    this.vService.delete(viewId);
   }
 }
--- src/app/services/view-service/view.service.ts
+++ src/app/services/view-service/view.service.ts
@@ -1,51 +1,54 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { NodeView } from '../../../assets/types';
+import { NodeView, ViewPosition } from '../../../assets/types';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ViewService{
-  private readonly url: string;
-  items: Array<NodeView>;
-  counterId: number;
+export class ViewService
+{
+  private readonly items: Array<NodeView>;
+  private counterId: number;
 
-  constructor(private db: HttpClient){
-    this.url = '../assets/main-zone-objects.json';
+  constructor()
+  {
     this.items = Array();
     this.counterId = 0;
   }
 
-  getViews(): Observable<Array<NodeView>>{
-    return this.db.get<Array<NodeView>>(this.url);
-  }
-
-  getAll(): Array<NodeView>{
-    // TODO : debounce the load. Usage in main-zone..
+  getAll(): ReadonlyArray<NodeView>
+  {
     console.log('ViewService.getAll()');
+
     return this.items;
   }
+  add(nodeTypeId: number, nodeTypeName: string): void
+  {
+    console.log(`ViewService.add(${nodeTypeId}, ${nodeTypeName})`);
 
-  add(nodeTypeId: number, nodeTypeName: string): void{
-    // TODO : fix locaation when added.
     const item: NodeView = {
       viewId: this.counterId,
       nodeId: nodeTypeId,
       name: nodeTypeName,
-      position: {x: 0, y: 0},
+      position: { x: 0, y: 0 },
       address: (this.counterId + 1) * 1000
     };
     this.counterId++;
     this.items.push(item);
   }
+  update(itemId: number, distance: ViewPosition): ViewPosition
+  {
+    console.log(`ViewService.update(${itemId}, ${distance})`);
 
-  update(itemId: number, distance: { x: number, y: number }): { x: number; y: number }{
     const index = this.items.findIndex(item => item.viewId === itemId);
-    if (!isNaN(index)){
-      this.items[index].position.x += distance.x;
-      this.items[index].position.y += distance.y;
-    }
+    this.items[index].position.x += distance.x;
+    this.items[index].position.y += distance.y;
     return this.items[index].position;
+  }
+  delete(itemId: number): void
+  {
+    console.log(`ViewService.delete(${itemId})`);
+
+    const index = this.items.findIndex(item => item.viewId === itemId);
+    this.items.splice(index, 1);
   }
 }
