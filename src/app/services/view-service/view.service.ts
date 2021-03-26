96a38c2da4ca29c8ebe6a426197c4f1b2091dcf8
AMIT-DESKT\amit_shwarts <amit.shwarts@gmail.com>
2021-03-26 18:41:40 +0300

    naming convention

--- src/app/services/view-service/view.service.ts
+++ src/app/services/view-service/view.service.ts
@@ -6,49 +6,48 @@ import { NodeView, ViewPosition } from '../../../assets/types';
 })
 export class ViewService
 {
-  private readonly items: Array<NodeView>;
-  private counterId: number;
+  private readonly r_Items: Array<NodeView>;
+  private m_CounterId: number;
 
   constructor()
   {
-    this.items = Array();
-    this.counterId = 0;
+    this.r_Items = new Array();
+    this.m_CounterId = 0;
   }
 
   getAll(): ReadonlyArray<NodeView>
   {
     console.log('ViewService.getAll()');
-
-    return this.items;
+    return this.r_Items;
   }
   add(nodeTypeId: number, nodeTypeName: string): void
   {
     console.log(`ViewService.add(${nodeTypeId}, ${nodeTypeName})`);
 
     const item: NodeView = {
-      viewId: this.counterId,
+      viewId: this.m_CounterId,
       nodeId: nodeTypeId,
       name: nodeTypeName,
       position: { x: 0, y: 0 },
-      address: (this.counterId + 1) * 1000
+      address: (this.m_CounterId + 1) * 1000
     };
-    this.counterId++;
-    this.items.push(item);
+    this.m_CounterId++;
+    this.r_Items.push(item);
   }
   update(itemId: number, distance: ViewPosition): ViewPosition
   {
     console.log(`ViewService.update(${itemId}, ${distance})`);
 
-    const index = this.items.findIndex(item => item.viewId === itemId);
-    this.items[index].position.x += distance.x;
-    this.items[index].position.y += distance.y;
-    return this.items[index].position;
+    const index = this.r_Items.findIndex(item => item.viewId === itemId);
+    this.r_Items[index].position.x += distance.x;
+    this.r_Items[index].position.y += distance.y;
+    return this.r_Items[index].position;
   }
   delete(itemId: number): void
   {
     console.log(`ViewService.delete(${itemId})`);
 
-    const index = this.items.findIndex(item => item.viewId === itemId);
-    this.items.splice(index, 1);
+    const index = this.r_Items.findIndex(item => item.viewId === itemId);
+    this.r_Items.splice(index, 1);
   }
 }
